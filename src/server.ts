d15cf8804afc7ccfd040e913079b3c5d696b54ac
Afjal Hossain <abdullahalafjal31@gmail.com>
2026-08-10 13:38:26 +0600

    Added README.md filw

--- src/server.ts
+++ src/server.ts
@@ -19,13 +19,13 @@ const PORT = process.env.PORT || 5000;
 async function bootstrap() {
   try {
     await prisma.$connect();
-    console.log('📦 Successfully connected to the database.');
+    console.log('Successfully connected to the database.');
 
     app.listen(PORT, () => {
       console.log(`🚀 Server is running on http://localhost:${PORT}`);
     });
   } catch (error) {
-    console.error('❌ Failed to connect to the database:', error);
+    console.error('Failed to connect to the database:', error);
     process.exit(1);
   }
 }
