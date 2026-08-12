a8836f8359bfe9a2401d41e9fd51c943db278a08
Afjal Hossain <abdullahalafjal31@gmail.com>
2026-08-12 15:58:05 +0600

    feat: serve index.html UI on root route

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
+import path from 'path';
 import express from 'express';
 import type { Application, Request, Response } from 'express';
 import cors from 'cors';
 
 // সবগুলো রাউটের শেষে .js যুক্ত করা হলো
+
 import authRoutes from './routes/auth.routes.js';
 import categoryRoutes from './routes/category.routes.js';
 import productRoutes from './routes/product.routes.js';
@@ -23,12 +25,16 @@ app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
 
 // Health Check
-app.get('/', (req: Request, res: Response) => {
-  res.status(200).json({
-    success: true,
-    message: 'Welcome to the SCIC/EJP-13 Backend API',
-    data: {}
-  });
+// app.get('/', (req: Request, res: Response) => {
+//   res.status(200).json({
+//     success: true,
+//     message: 'Welcome to the SCIC/EJP-13 Backend API',
+//     data: {}
+//   });
+// });
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(process.cwd(), 'index.html'));
 });
 
 export default app;
